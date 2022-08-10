61d52984adc35d64184b3b2b43b824266a00969f
labor-day <shaffer.scott@yahoo.com>
2022-08-10 16:51:16 -0600

    complete

--- server/app.js
+++ server/app.js
@@ -2,7 +2,23 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('GET / This is the root URL');
+  return res.send('GET / This is the root URL');
+});
+
+
+//middleware after all route handling which creates an error if no route was found
+app.use((req, res, next) => {
+  let error = new Error("Sorry, the requested resource couldn't be found");
+  error.statusCode = 404;
+  next(error);
+})
+
+// Custom error handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.statusCode = (err.statusCode || 500);
+  resBody = {"message": err.message, "statusCode": res.statusCode};
+  res.send(resBody);
 });
 
 const port = 5000;
